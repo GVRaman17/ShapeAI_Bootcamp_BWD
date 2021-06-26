b9d9e79885ccc03367e64524328b00b45982e83a
GVRaman17 <86295246+GVRaman17@users.noreply.github.com>
2021-06-26 19:48:35 +0530

    Add files via upload
    java script

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>It is my project</h1>
+      <p>Name: G.Venkatramamoorthy<br/>course: Java script<br/>Project submission</p>
+    </div>
+  );
+}
+
+export default Info;
